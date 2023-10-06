284b402238a06e6d65d5b5e4b2c573a8592af458
Jean Carlos <omashu10k@gmail.com>
2023-10-06 13:05:47 -0300

    cambios

--- src/app/alumnos/alumnos.page.ts
+++ src/app/alumnos/alumnos.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alumnos.page.scss'],
 })
 export class AlumnosPage implements OnInit {
+  alumnos: any[] = []; // Aquí deberías cargar tu lista de alumnos desde algún origen de datos
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // Simula la carga de la lista de alumnos (reemplaza esto con tu lógica real)
+    this.cargarAlumnos();
   }
 
+  ngOnInit() {}
+
+  cargarAlumnos() {
+    // Aquí debes cargar la lista de alumnos desde tu API o base de datos
+    // Ejemplo: Supongamos que obtienes los datos de una API
+    // this.miServicio.obtenerAlumnos().subscribe((data) => {
+    //   this.alumnos = data;
+    // });
+  }
 }
